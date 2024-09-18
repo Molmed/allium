1913632e23325c1dc319f24486908e2fc243143b
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2024-09-18 14:29:28 +0200

    Add script for generating allium metadata annotation file

--- src/preprocessing/1_download_ref.py
+++ src/preprocessing/1_download_ref.py
@@ -1,10 +1,9 @@
 import os
 import requests
-import gzip
 
 # Define the URL of the reference genome
-REFERENCE_GENOME = 'Homo_sapiens.GRCh38.103.gtf'
-REFERENCE_GENOME_URL = f'http://ftp.ensembl.org/pub/release-103/gtf/homo_sapiens/{REFERENCE_GENOME}.gz'
+REFERENCE_GENOME = 'Homo_sapiens.GRCh38.103.gtf.gz'
+REFERENCE_GENOME_URL = f'http://ftp.ensembl.org/pub/release-103/gtf/homo_sapiens/{REFERENCE_GENOME}'
 
 # Define the directory to save the file relative to the script's directory
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -15,29 +14,19 @@ os.makedirs(DATA_DIR, exist_ok=True)
 
 # If reference file already exists, say so and exit
 gtf_file_path = os.path.join(DATA_DIR, REFERENCE_GENOME)
-gz_file_path = f'{gtf_file_path}.gz'
 
 if os.path.exists(gtf_file_path):
     print(f"{REFERENCE_GENOME} already exists at {gtf_file_path}. Done.")
     exit(0)
 
-if os.path.exists(gz_file_path):
-    print(f"{REFERENCE_GENOME}.gz already exists at {gz_file_path}. Skipping download.")
-else:
-    print(f"Downloading reference from {REFERENCE_GENOME_URL} to {DATA_DIR}...")
-    # Download the file
-    response = requests.get(REFERENCE_GENOME_URL, stream=True)
-    response.raise_for_status()  # Check if the request was successful
+print(f"Downloading reference from {REFERENCE_GENOME_URL} to {DATA_DIR}...")
+# Download the file
+response = requests.get(REFERENCE_GENOME_URL, stream=True)
+response.raise_for_status()  # Check if the request was successful
 
-    # Save the file to the specified directory
-    with open(gz_file_path, 'wb') as file:
-        for chunk in response.iter_content(chunk_size=8192):
-            file.write(chunk)
+# Save the file to the specified directory
+with open(gtf_file_path, 'wb') as file:
+    for chunk in response.iter_content(chunk_size=8192):
+        file.write(chunk)
 
-    print(f"Downloaded {REFERENCE_GENOME}.gz to {gz_file_path}")
-
-# Unzip the file
-print(f"Unzipping {REFERENCE_GENOME}.gz to {gtf_file_path}")
-with gzip.open(gz_file_path, 'rb') as gz_file:
-    with open(gtf_file_path, 'wb') as file:
-        file.write(gz_file.read())
+print(f"Downloaded {REFERENCE_GENOME}.gz to {gtf_file_path}")
--- src/preprocessing/2_parse_ref.r
+++ src/preprocessing/2_parse_ref.r
@@ -2,20 +2,59 @@ library(Rsubread)
 library(sva)
 library(ballgown)
 
-# Function to get the directory of the current script
-get_script_dir <- function() {
-  cmdArgs <- commandArgs(trailingOnly = FALSE)
-  match <- grep("--file=", cmdArgs)
-  if (length(match) > 0) {
-    return(dirname(normalizePath(sub("--file=", "", cmdArgs[match]))))
-  } else {
-    return(dirname(normalizePath(sys.frames()[[1]]$ofile)))
-  }
-}
+REF_VERSION <- "Homo_sapiens.GRCh38.103"
+REF_DATA_DIR <- file.path(getwd(), "data/reference")
+GTF_FILE_NAME <- paste0(REF_VERSION, ".gtf.gz")
+GENE_ANNOTATION_GTF <- file.path(REF_DATA_DIR, GTF_FILE_NAME)
+PROCESSED_ANNOT_FILE_NAME <- paste0(REF_VERSION, ".allium.annotations.csv")
+PROCESSED_ANNOT_PATH <- file.path(REF_DATA_DIR, PROCESSED_ANNOT_FILE_NAME)
 
-# Define a constant REF_GENOME relative to the script's directory
-script_dir <- get_script_dir()
-REF_GENOME <- file.path(script_dir, "../../data/reference/Homo_sapiens.GRCh38.103.gtf")
+cat("Gene annotation path:", GENE_ANNOTATION_GTF, "\n")
 
-# Print the constant REF_GENOME
-cat("Reference Genome Path:", REF_GENOME, "\n")
+SAF <- Rsubread::flattenGTF(GENE_ANNOTATION_GTF)
+GeneLength <- rowsum(SAF$End-SAF$Start+1, SAF$GeneID)
+annot <- gffRead(GENE_ANNOTATION_GTF)
+
+# Retrieve the attributes
+annot$gene_id = getAttributeField(annot$attributes,
+    field = "gene_id")
+
+annot$gene_name = getAttributeField(annot$attributes,
+    field = "gene_name")
+
+annot$gene_biotype = getAttributeField(annot$attributes,
+    field = "gene_biotype")
+
+tokeep <- c("seqname", "feature", "gene_id", "gene_name", "gene_biotype")
+annot_filtered <- annot[tokeep]
+
+# Remove quotes
+annot_filtered[] <- lapply(annot_filtered, gsub, pattern='"', replacement='')
+
+# Keep only genes
+annot_filtered <- annot_filtered[annot_filtered$feature == 'gene', ]
+
+# Convert gene lengths to dataframe
+gene_lengths <- as.data.frame(GeneLength)
+gene_lengths$gene_id <- rownames(GeneLength)
+
+# Reorder columns to have gene_id as the first column
+gene_lengths <- gene_lengths[, c("gene_id", names(gene_lengths)[-ncol(gene_lengths)])]
+
+# Name the columns
+colnames(gene_lengths) <- c("gene_id", "gene_length")
+
+# Join DataFrames on 'gene_id' column
+full_annot <- merge(annot_filtered, gene_lengths, by = "gene_id", all = TRUE)
+
+# Rename columns 
+colnames(full_annot) <- c("id", "chr", "feature", "name", "biotype", "length")
+
+# Drop feature column
+full_annot$feature <- NULL
+
+# Strip trailing semicolons from the biotype column
+full_annot$biotype <- sub(";$", "", full_annot$biotype)
+
+# Write processed metadata
+write.table(full_annot, PROCESSED_ANNOT_PATH, col.names = TRUE, row.names = FALSE, sep = ',', quote = FALSE)
