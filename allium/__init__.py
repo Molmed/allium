ba899f2b54bcf67998b5c266c18a248b6c194d39
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2024-09-23 19:52:16 +0200

    Fix imports

---
+++ allium/__init__.py
@@ -0,0 +1,5 @@
+from .allium_classifier import AlliumClassifier
+from .dnam_classifier import DNAMClassifier
+from .gex_classifier import GEXClassifier
+from .modality import GEX, DNAM
+from .subtype import Subtype
