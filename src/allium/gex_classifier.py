922da2965492913348552c67f51928733b26e917
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2024-09-23 09:42:49 +0200

    Remove outdated preprocess function

--- src/allium/gex_classifier.py
+++ src/allium/gex_classifier.py
@@ -12,19 +12,11 @@ class GEXClassifier(AlliumClassifier):
         self._model = joblib.load(models_path(f'allium_gex_{version}.joblib'))
         self._signatures = pd.read_csv(signatures_path(f'signature_genes_{version}.csv'))
 
-    @staticmethod
-    def preprocess_phenotypes(pheno):
-        if pheno is None:
-            return None
-        pheno['Subtypes'] = [name.split('_')[0] for name in pheno.Subtype_name]
-        pheno.Subtypes.replace(['ETV6-RUNX1', 'Hyperdiploid', 'DUX4-ERG'], ['t(12;21)', 'HeH', 'DUX4-r'], inplace = True)
-        return pheno
-
     def predict(self, gex, pheno = None, to_json = False, known_subtype_col = 'Subtypes'):
         return self.predictionsNSC(subtype_groups = Subtype.all(GEX),
                             model = self._model,
                             discoverydf = gex,
-                            discoverypheno = GEXClassifier.preprocess_phenotypes(pheno),
+                            discoverypheno = pheno,
                             clinicaldatalist = [known_subtype_col] if not pheno is None else None,
                             unique_genedf = self._signatures,
                             subtypecol = 'Subtype',
