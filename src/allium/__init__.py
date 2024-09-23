0eab95332794cfe1723a5dfb0d4d27b2069cfc69
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2024-09-23 20:17:59 +0200

    Only import essentials

--- src/allium/__init__.py
+++ src/allium/__init__.py
@@ -1,5 +1,2 @@
-from .allium_classifier import AlliumClassifier
-from .dnam_classifier import DNAMClassifier
-from .gex_classifier import GEXClassifier
 from .modality import GEX, DNAM
 from .subtype import Subtype
