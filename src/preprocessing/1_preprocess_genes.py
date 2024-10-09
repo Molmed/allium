e3aa3fbf9ad79e2d7f50328c466d6c8abf2af0e8
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2024-10-09 13:42:22 +0200

    Ugrade gene thesaurus, and generalize preprocessing script

--- src/preprocessing/1_preprocess_genes.py
+++ src/preprocessing/1_preprocess_genes.py
@@ -6,9 +6,13 @@ gt = GeneThesaurus(data_dir='/tmp')
 
 # TODO: make these command line parameters
 # GEX data from Lilljebjorn et al. 2016
-path_to_data = '/Users/marly389/Data/lilljebjorn/'
-input_file = f'{path_to_data}/counts.raw.lilljebjorn.csv'
-output_file = f'{path_to_data}/counts.filtered.lilljebjorn.csv'
+path_to_data = '/home/mariya/Data/tran'
+input_file = f'{path_to_data}/counts.raw.tran.csv'
+output_file = f'{path_to_data}/counts.filtered.tran.csv'
+
+# For Tran: "16-"
+# For Lilljebjorn: "Case"
+SAMPLE_COL_PREFIX = '16-'
 
 # Reference
 REFERENCE_GENOME = 'Homo_sapiens.GRCh38.103'
@@ -22,20 +26,22 @@ data = pd.read_csv(input_file, index_col=0)
 ref = pd.read_csv(ANNOT_FILE_FILTERED)
 
 # TODO: add command-line parameters to determine if translation from ensembl id is necessary
+
 # Get gene names from ensembl ids
-# translated_genes = gt.translate_genes(data.index.values,
-#                                       source='ensembl_id',
-#                                       target='symbol')
-# data['gene_name_std'] = data.index.map(translated_genes).fillna("")
+translated_genes = gt.translate_genes(data.index.values,
+                                      source='ensembl_id',
+                                      target='symbol')
+data['gene_name_std'] = data.index.map(translated_genes).fillna("")
 
+# Code to use if index is in symbol format
 # Get all gene name values to update to the latest standard
-updated_genes = gt.update_gene_symbols(data.index.values)
+# updated_genes = gt.update_gene_symbols(data.index.values)
 
-# Copy data.index to gene_name_std
-data['gene_name_std'] = data.index
+# # Copy data.index to gene_name_std
+# data['gene_name_std'] = data.index
 
 # Update gene_name_std from updated_genes
-data['gene_name_std'] = data['gene_name_std'].map(updated_genes).fillna(data['gene_name_std'])
+# data['gene_name_std'] = data['gene_name_std'].map(updated_genes).fillna(data['gene_name_std'])
 
 # Do the same for ref
 updated_genes = gt.update_gene_symbols(ref['name'].values)
@@ -57,7 +63,7 @@ duplicate_gene_names = duplicates['gene_name_std'].unique()
 
 # For each row in duplicates, sum the values of the rows with the same name
 # and update the row with the sum. Then drop the duplicates.
-case_columns = [col for col in duplicates.columns if col.startswith("Case")]
+case_columns = [col for col in duplicates.columns if col.startswith(SAMPLE_COL_PREFIX)]
 for duplicate_gene in duplicate_gene_names:
     # Get all rows with the same id
     dupes = data[data['gene_name_std'] == duplicate_gene]
