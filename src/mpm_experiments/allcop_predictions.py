5586eadd80fb8164f10fc058c7d8c5d85c992221
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2024-10-15 16:27:34 +0200

    Add allcop predictions file

---
+++ src/mpm_experiments/allcop_predictions.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from src.allium.gex_classifier import GEXClassifier
+
+datasets = ['jude', 'lilljebjorn', 'jude']
+data_dir = '/home/mariya/Data/allium'
+gc = GEXClassifier('v3')
+
+for dataset in datasets:
+    print("Predicting for", dataset)
+    testX = pd.read_csv(f'{data_dir}/{dataset}.counts.allium.csv',
+                        index_col="id")
+    pheno = pd.read_csv(f'{data_dir}/{dataset}.pheno.allium.csv',
+                        index_col="id",
+                        sep=';')
+    result = gc.get_predictions(testX, pheno=pheno)
+    result.to_csv(f'{data_dir}/{dataset}.predictions.csv')
