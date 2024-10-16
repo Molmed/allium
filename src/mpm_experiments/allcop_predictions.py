4eea28bd13aaa7e5d9e2ca73dbf89d1f21fea955
Mariya Lysenkova Wiklander <mariya.lysenkova@medsci.uu.se>
2024-10-16 08:41:02 +0200

    Fix allcop prediction logic

--- src/mpm_experiments/allcop_predictions.py
+++ src/mpm_experiments/allcop_predictions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from src.allium.gex_classifier import GEXClassifier
 
-datasets = ['jude', 'lilljebjorn', 'jude']
+datasets = ['jude', 'lilljebjorn', 'tran']
 data_dir = '/home/mariya/Data/allium'
 gc = GEXClassifier('v3')
 
